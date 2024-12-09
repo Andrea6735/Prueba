14c8b4bf995c9af6659ea8bbb9a39157911713e7
Andrea6735 <usuario890075@gmail.com>
2024-12-09 18:48:24 -0300

    Update verificar.php

--- view/home/verificar.php
+++ view/home/verificar.php
@@ -7,7 +7,7 @@
     $bandera = $obj->verificarusuario($correo,$contraseña);
     if($bandera){
         $_SESSION['usuario'] = $correo;
-        header("Location:panel_control.php");
+        header("Location:profile-card/index.html");
     }else{
         $error = "<li>Las claves son incorrectas</li>";
         header("Location:login.php?error=".$error);

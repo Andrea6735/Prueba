952a525a232ee5fba9d0dbf476690f68ddc42adf
Andrea6735 <usuario890075@gmail.com>
2024-12-09 18:41:41 -0300

    Update store.php

--- view/home/store.php
+++ view/home/store.php
@@ -4,10 +4,47 @@
     $correo = $_POST['correo'];
     $contraseña = $_POST['contraseña'];
     $confirmarContraseña = $_POST['confirmarContraseña'];
+    $rut = $_POST['RUT'];
     $error = "";
+
+    function validarRUT($RUT) {
+    
+    $RUT = strtoupper(preg_replace('/[^0-9K]/', '', $RUT));
+
+    if (!preg_match('/^[0-9]{7,8}[0-9K]$/', $RUT)) {
+        return false;
+    }
+
+    $numero = substr($RUT, 0, -1);
+    $dv = substr($RUT, -1);
+
+    $suma = 0;
+    $multiplo = 2;
+
+    for ($i = strlen($numero) - 1; $i >= 0; $i--) {
+        $suma += $numero[$i] * $multiplo;
+        $multiplo = $multiplo < 7 ? $multiplo + 1 : 2;
+    }
+
+    $resto = $suma % 11;
+    $dvCalculado = 11 - $resto;
+
+    if ($dvCalculado == 11) {
+        $dvCalculado = '0';
+    } elseif ($dvCalculado == 10) {
+        $dvCalculado = 'K';
+    } else {
+        $dvCalculado = (string) $dvCalculado;
+    }
+
+    return $dvCalculado === $dv;
+}
+
     if(empty($correo) && empty($contraseña) && empty($confirmarContraseña)){
         $error .= "<li>Los campos son iguales</li>";
         header("Location:signup.php?error=".$error."&&correo=".$correo."&&contraseña=".$contraseña."&&confirmarContraseña=".$confirmarContraseña);
+
+        
     }else if($correo || $contraseña || $confirmarContraseña){
         if($contraseña == $confirmarContraseña){
             if($obj->guardarUsuario($correo,$contraseña) == false){
